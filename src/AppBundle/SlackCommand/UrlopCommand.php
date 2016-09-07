c37ef1592a8db9cb601f40daef51c0031369cd4c
Szymon Skórczyński <szymon.skorczynski@schibsted.pl>
2016-09-07 15:20:02 +0200

    Dates parsing

--- src/AppBundle/SlackCommand/UrlopCommand.php
+++ src/AppBundle/SlackCommand/UrlopCommand.php
@@ -17,22 +17,34 @@ class UrlopCommand extends AbstractCommand implements TakeDelegation\Responder
 
         $urlopRegex = Regex::match('/urlop (.+)/', $message);
         if ($urlopRegex->hasMatch()) {
+            preg_match('/urlop(?<date>.+)/', $message, $results);
+            $period = $this->getPeriod($results['date']);
+
             $this->reply('Udanego wypoczynku! :) :sunny:');
         }
 
         $delegacjaRegex = Regex::match('/delegacja (.+)/', $message);
         if ($delegacjaRegex->hasMatch()) {
+            preg_match('/delegacja(?<date>.+)/', $message, $results);
+            $period = $this->getPeriod($results['date']);
+
             $useCase = new TakeDelegation(new AbsenceStorage());
-            $useCase->execute(new TakeDelegation\Command('ulff', '8.09.2016'), $this);
+            $useCase->execute(new TakeDelegation\Command($user->getUsername(), $period['startDate']), $this);
         }
 
         $wfhRegex = Regex::match('/wfh (.+)/', $message);
         if ($wfhRegex->hasMatch()) {
+            preg_match('/wfh(?<date>.+)/', $message, $results);
+            $period = $this->getPeriod($results['date']);
+
             $this->reply('Mam nadzieję, że jednak będziesz pracował ;) :house:');
         }
 
         $zwolnienieRegex = Regex::match('/zwolnienie (.+)/', $message);
         if ($zwolnienieRegex->hasMatch()) {
+            preg_match('/zwolnienie(?<date>.+)/', $message, $results);
+            $period = $this->getPeriod($results['date']);
+
             $this->reply('Szybkiego powrotu do zdrowia! :face_with_thermometer:');
         }
     }
@@ -52,5 +64,42 @@ class UrlopCommand extends AbstractCommand implements TakeDelegation\Responder
     public function failedToTakeDelegation(AbsenceException $exception)
     {
         $this->reply('Coś się zjebało :(');
+    }
+
+    private function getPeriod(string $date)
+    {
+        $date = trim($date);
+
+        $startDate = null;
+        $endDate = null;
+
+        if ($date === 'jutro') {
+            $startDate = new \DateTime('+1 day');
+        }
+
+        if ($date === 'dziś' || $date === 'dzis' || $date === 'dzisiaj') {
+            $startDate = new \DateTime('now');
+        }
+
+        $datesRegex = Regex::match('/(\d{1,2}).(\d{1,2})-(\d{1,2}).(\d{1,2})/', $date);
+        if ($datesRegex->hasMatch()) {
+            $dayStart = $datesRegex->group(1);
+            $monthStart = $datesRegex->group(2);
+            $dayEnd = $datesRegex->group(3);
+            $monthEnd = $datesRegex->group(4);
+
+            $startDate = new \DateTime($monthStart . '/' . $dayStart);
+            $endDate = new \DateTime($monthEnd . '/' . $dayEnd);
+        }
+
+        $datesRegex = Regex::match('/(\d{1,2}).(\d{1,2})/', $date);
+        if ($datesRegex->hasMatch()) {
+            $dayStart = $datesRegex->group(1);
+            $monthStart = $datesRegex->group(2);
+
+            $startDate = new \DateTime($monthStart . '/' . $dayStart);
+        }
+
+        return ['startDate' => $startDate, 'endDate' => $endDate];
     }
 }
