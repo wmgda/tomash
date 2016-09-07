b1a832be9aa875fe473ece58e70fbb86ce926338
Remigiusz Specht <tetsuobe+github@gmail.com>
2016-09-07 14:16:15 +0200

    Delegation usecase

--- src/Domain/UseCase/Absence/TakeDelegation.php
+++ src/Domain/UseCase/Absence/TakeDelegation.php
@@ -3,14 +3,42 @@ declare(strict_types = 1);
 
 namespace Domain\UseCase\Absence;
 
+use Domain\Exception\AbsenceException;
+use Domain\Model\Absence\Absence;
 use Domain\UseCase\Absence\TakeDelegation\Command;
 use Domain\UseCase\Absence\TakeDelegation\Responder;
+use Infrastructure\File\AbsenceStorage;
 
 class TakeDelegation
 {
-    public function execute(Command $command, Responder $responder)
-    {
+    /**
+     * @var AbsenceStorage
+     */
+    private $storage;
 
+    /**
+     * TakeDelegation constructor.
+     * @param AbsenceStorage $absenceStorage
+     */
+    public function __construct(AbsenceStorage $absenceStorage)
+    {
+        $this->storage = $absenceStorage;
     }
 
+
+    public function execute(Command $command, Responder $responder)
+    {
+        try {
+            $absence = new Absence(
+                $command->getPerson(),
+                $command->getDate(),
+                Absence::ABSENCE_TYPE_DELEGATION
+            );
+            $this->storage->add($absence);
+        } catch (AbsenceException $exception) {
+            return $responder->failedToTakeDelegation($exception);
+        }
+
+        return $responder->delegationTakenSuccessfully();
+    }
 }
--- src/Domain/UseCase/Absence/TakeDelegation/Command.php
+++ src/Domain/UseCase/Absence/TakeDelegation/Command.php
@@ -5,5 +5,40 @@ namespace Domain\UseCase\Absence\TakeDelegation;
 
 class Command
 {
+    /**
+     * @var string
+     */
+    private $person;
 
+    /**
+     * @var string
+     */
+    private $date;
+
+    /**
+     * Command constructor.
+     * @param string $person
+     * @param string $date
+     */
+    public function __construct(string $person, string $date)
+    {
+        $this->person = $person;
+        $this->date = $date;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPerson(): string
+    {
+        return $this->person;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDate(): string
+    {
+        return $this->date;
+    }
 }
