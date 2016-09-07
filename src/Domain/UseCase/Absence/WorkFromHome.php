b707fa9490ef3a6518affaece6dfda58f3fc1236
Remigiusz Specht <tetsuobe+github@gmail.com>
2016-09-07 11:23:46 +0200

    Absence use cases

---
+++ src/Domain/UseCase/Absence/WorkFromHome.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types = 1);
+
+namespace Domain\UseCase\Absence;
+
+use Domain\UseCase\Absence\WorkFromHome\Command;
+use Domain\UseCase\Absence\WorkFromHome\Responder;
+
+class WorkFromHome
+{
+    public function execute(Command $command, Responder $responder)
+    {
+
+    }
+
+}
