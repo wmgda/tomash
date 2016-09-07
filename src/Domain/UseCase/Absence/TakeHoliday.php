ce64701d782e64c646e00957c0eae43ba984c6e3
Remigiusz Specht <tetsuobe+github@gmail.com>
2016-09-07 18:04:53 +0200

    Take holiday use case

--- src/Domain/UseCase/Absence/TakeHoliday.php
+++ src/Domain/UseCase/Absence/TakeHoliday.php
@@ -3,13 +3,42 @@ declare(strict_types = 1);
 
 namespace Domain\UseCase\Absence;
 
+use Domain\Exception\AbsenceException;
+use Domain\Model\Absence\Absence;
 use Domain\UseCase\Absence\TakeHoliday\Command;
 use Domain\UseCase\Absence\TakeHoliday\Responder;
+use Infrastructure\File\AbsenceStorage;
 
 class TakeHoliday
 {
+    /**
+     * @var AbsenceStorage
+     */
+    private $storage;
+
+    /**
+     * TakeDelegation constructor.
+     * @param AbsenceStorage $absenceStorage
+     */
+    public function __construct(AbsenceStorage $absenceStorage)
+    {
+        $this->storage = $absenceStorage;
+    }
+
     public function execute(Command $command, Responder $responder)
     {
+        try {
+            $absence = new Absence(
+                Absence::ABSENCE_TYPE_HOLIDAY,
+                $command->getPerson(),
+                $command->getDateStart(),
+                $command->getDateEnd()
+            );
+            $this->storage->add($absence);
+        } catch (AbsenceException $exception) {
+            return $responder->failedToTakeHoliday($exception);
+        }
 
+        return $responder->holidayTakenSuccessfully();
     }
 }
--- src/Domain/UseCase/Absence/TakeHoliday/Command.php
+++ src/Domain/UseCase/Absence/TakeHoliday/Command.php
@@ -5,5 +5,55 @@ namespace Domain\UseCase\Absence\TakeHoliday;
 
 class Command
 {
+    /**
+     * @var string
+     */
+    private $person;
 
+    /**
+     * @var \DateTime
+     */
+    private $dateStart;
+
+    /**
+     * @var \DateTime
+     */
+    private $dateEnd;
+
+    /**
+     * Command constructor.
+     * @param string $person
+     * @param \DateTime $dateStart
+     * @param \DateTime $dateEnd
+     */
+    public function __construct(string $person, \DateTime $dateStart, \DateTime $dateEnd)
+    {
+        $this->person = $person;
+        $this->dateStart = $dateStart;
+        $this->dateEnd = $dateEnd;
+    }
+
+    /**
+     * @return string
+     */
+    public function getPerson(): string
+    {
+        return $this->person;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateStart(): \DateTime
+    {
+        return $this->dateStart;
+    }
+
+    /**
+     * @return \DateTime
+     */
+    public function getDateEnd(): \DateTime
+    {
+        return $this->dateEnd;
+    }
 }
