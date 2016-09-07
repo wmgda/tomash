2ec40a5ff85c0ff1c016ccda9d19334171e5c300
ulff <ulff.pl@gmail.com>
2016-09-07 15:38:02 +0200

    implementation of order storage

---
+++ src/Infrastructure/File/OrderStorage.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Infrastructure\File;
+
+use Domain\Model\Lunch\Order;
+use Domain\Storage\string;
+
+class OrderStorage implements \Domain\Storage\OrderStorage
+{
+    private static $path = './var/storage/orders/';
+
+    public function save(Order $order)
+    {
+        $file = static::$path . $order->getRestaurant()->getName();
+
+        file_put_contents($file, serialize($order));
+    }
+
+    public function load(string $restaurantName)
+    {
+        $file = static::$path . $restaurantName;
+
+        if(!file_exists($file)) {
+            throw new \Exception(sprintf("Order from restaurant %s does not exist", $restaurantName));
+        }
+
+        return unserialize(file_get_contents($file));
+    }
+
+    public function drop(string $restaurantName)
+    {
+        $file = static::$path . $restaurantName;
+
+        unlink($file);
+    }
+
+}
