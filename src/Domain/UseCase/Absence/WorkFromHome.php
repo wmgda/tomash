7aff4743e261ad6ff1464a5d092fa5c59e23a7c6
Remigiusz Specht <tetsuobe+github@gmail.com>
2016-09-07 18:20:13 +0200

    Work from home usecase

--- src/Domain/UseCase/Absence/WorkFromHome.php
+++ src/Domain/UseCase/Absence/WorkFromHome.php
@@ -3,14 +3,42 @@ declare(strict_types = 1);
 
 namespace Domain\UseCase\Absence;
 
+use Domain\Exception\AbsenceException;
+use Domain\Model\Absence\Absence;
 use Domain\UseCase\Absence\WorkFromHome\Command;
 use Domain\UseCase\Absence\WorkFromHome\Responder;
+use Infrastructure\File\AbsenceStorage;
 
 class WorkFromHome
 {
-    public function execute(Command $command, Responder $responder)
-    {
+    /**
+     * @var AbsenceStorage
+     */
+    private $storage;
 
+    /**
+     * TakeDelegation constructor.
+     * @param AbsenceStorage $absenceStorage
+     */
+    public function __construct(AbsenceStorage $absenceStorage)
+    {
+        $this->storage = $absenceStorage;
     }
 
+    public function execute(Command $command, Responder $responder)
+    {
+        try {
+            $absence = new Absence(
+                Absence::ABSENCE_TYPE_WORK_FROM_HOME,
+                $command->getPerson(),
+                $command->getDateStart(),
+                $command->getDateEnd()
+            );
+            $this->storage->add($absence);
+        } catch (AbsenceException $exception) {
+            return $responder->failedToWorkFormHome($exception);
+        }
+
+        return $responder->workFromHomeSuccessfully();
+    }
 }
--- src/Domain/UseCase/Absence/WorkFromHome/Responder.php
+++ src/Domain/UseCase/Absence/WorkFromHome/Responder.php
@@ -3,7 +3,11 @@ declare(strict_types = 1);
 
 namespace Domain\UseCase\Absence\WorkFromHome;
 
+use Domain\Exception\AbsenceException;
+
 interface Responder
 {
+    public function failedToWorkFormHome(AbsenceException $exception);
 
+    public function workFromHomeSuccessfully();
 }
